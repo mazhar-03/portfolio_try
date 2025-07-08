e15bb4cefc13a671548935e0daf92b8d4d5460f2
kpn <kpnprod@gmail.com>
2025-07-08 13:42:35 +0200

    dummy data

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,32 @@
 export default function Projects() {
-  return <h1 className="text-gray-400">Projects Page</h1>;
+  return (
+    <section className="w-full">
+      <div className="container mx-auto">
+        <h1 className="text-gray-400">Projects Page</h1>
+
+        <div className="grid grid-cols-3 gap-4">
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+            
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+          <div className="w-full aspect-[9/16] bg-black">
+          </div>
+        </div>
+      </div>
+      
+    </section>
+    
+  
+  );
 }
