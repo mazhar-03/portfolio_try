3716682ef5f7f567708280a48c0aeb7e7d0f8b32
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-07-09 12:53:59 +0200

    docs are added works fine. new transcript will be uploaded

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,49 @@
-// import Image from "next/image";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <div>
-            <section className="h-screen flex items-center justify-center bg-green text-white">
-                Homepage section 1
-            </section>
+        <section className="min-h-screen flex flex-col md:flex-row items-center justify-center px-6 py-12 max-w-6xl mx-auto gap-12">
+            <div className="w-64 h-64 md:w-80 md:h-80 relative rounded-full overflow-hidden flex-shrink-0 shadow-lg">
+                <Image
+                    src="/static/image/my_photo.jpeg"
+                    alt="Mazhar Altınçay"
+                    className="object-cover"
+                    priority
+                    width={640}
+                    height={640}
+                />
+            </div>
 
-            <section className="h-screen flex items-center justify-center bg-darkblue text-white">
-                Homepage section 2
-            </section>
-        </div>
+            <div className="max-w-xl text-center space-y-4">
+                <p className=" font-bold text-gray-600">
+                    Hello, I’m
+                </p>
+                <h1 className="text-5xl text-gray-900">Mazhar Altınçay</h1>
+                <p className="text-lg text-gray-600">
+                    Software Engineering Student
+                </p>
+
+                <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
+                    <Link
+                        href="/static/docs/cv_resume2.pdf"
+                        download
+                        className="rounded-full border-2 border-black bg-white text-black font-semibold py-3 px-6 transition duration-300 ease-in-out text-center hover:bg-gray-200"
+                    >
+                        Download CV
+                    </Link>
+
+                    <Link
+                        href="/static/docs/transcript.pdf"
+                        download
+                        className="rounded-full bg-black text-white font-semibold py-3 px-6 transition duration-300 ease-in-out text-center hover:bg-gray-800"
+                    >
+                        Download Resume
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 }
