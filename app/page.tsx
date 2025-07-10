a4778d27b1a65244e6d258dc7023dc7feed86999
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-07-10 14:50:17 +0200

    home-page made dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -21,28 +21,29 @@ export default async function Home() {
 
             <div className="max-w-xl text-center space-y-4">
                 <p className=" font-bold text-gray-600">
-                    Hello, I’m
+                    {homepage?.introText}
                 </p>
-                <h1 className="text-5xl text-gray-900">Mazhar Altınçay</h1>
+                <h1 className="text-5xl text-gray-900">{homepage?.nameSurname}</h1>
                 <p className="text-lg text-gray-600">
-                    Software Engineering Student
+                    {homepage?.jobTitle}
                 </p>
 
                 <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
                     <Link
-                        href="/static/docs/cv_resume2.pdf"
+                        href={homepage?.button1Url}
                         download
                         className="rounded-full border-2 border-black bg-white text-black font-semibold py-3 px-6 transition duration-300 ease-in-out text-center hover:bg-gray-200"
                     >
-                        Download CV
+                        {homepage?.button1Text}
                     </Link>
 
                     <Link
+                      // did not manage to make it dynamic
                         href="/static/docs/transcript_4.pdf"
                         download
                         className="rounded-full bg-black text-white font-semibold py-3 px-6 transition duration-300 ease-in-out text-center hover:bg-gray-800"
                     >
-                        Download Transcript
+                        {homepage?.button2Text}
                     </Link>
                 </div>
             </div>
